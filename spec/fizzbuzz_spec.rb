c2576c27061123184bf14ea2b8b4788e8b8ac390
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-02 10:45:55 +0000

    Oops we finished it

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,38 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+    
+    it "returns 'fizz' when passed 3" do
+        expect(fizzbuzz(3)).to eq "Fizz"
+    end
+    
+    it "returns 'buzz' when passed 5" do
+        expect(fizzbuzz(5)).to eq "Buzz"
+    end
+
+    it "returns appropriate array when passed numbers 0-20" do
+        expected_result = ["1",
+            "2",
+            "Fizz",
+            "4",
+            "Buzz",
+            "Fizz",
+            "7",
+            "8",
+            "Fizz",
+            "Buzz",
+            "11",
+            "Fizz",
+            "13",
+            "14",
+            "FizzBuzz",
+            "16",
+            "17",
+            "Fizz",
+            "19",
+            "Buzz"]
+        result = []
+        (1..20).each { |number| result.push(fizzbuzz(number)) }
+        expect(result).to eq expected_result
+    end
+end
