630b4dbd499cf9546267da01f235e2ea043a5572
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-02 11:15:07 +0000

    Add test for 'FizzBuzz' output

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,16 @@ require "fizzbuzz"
 
 describe "fizzbuzz" do
     
-    it "returns 'fizz' when passed 3" do
+    it "returns 'Fizz' when passed 3" do
         expect(fizzbuzz(3)).to eq "Fizz"
     end
     
-    it "returns 'buzz' when passed 5" do
+    it "returns 'Buzz' when passed 5" do
         expect(fizzbuzz(5)).to eq "Buzz"
+    end
+
+    it "returns 'FizzBuzz' when passed 15" do
+        expect(fizzbuzz(15)).to eq "FizzBuzz"
     end
 
     it "returns appropriate array when passed numbers 0-20" do
